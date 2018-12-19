8bb7e31ee24bf3fd304e705da7d446fdacb1c3b4
kawayi1727 <https://github.com/kawayi1727/algorithm>
2018-12-19 22:24:33 +0800

    排序

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,9 +4,32 @@ namespace ConsoleApp1
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+        }
+
+        public int[] InsertionSort(int[] s)
+        {
+
+            {
+                for (int i = 0; i < s.Length - 1;)
+                {
+                    int b = s[i + 1];
+                    int j = i;
+                    while (j >= 0 && s[j] > b)
+                    {
+                        s[j + 1] = s[j];
+                        j--;
+                    }
+                    s[j + 1] = b;
+                    Console.WriteLine();
+                    Console.ReadKey();
+                }
+                return s;
+            }
+         
         }
     }
 }
